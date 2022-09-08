b0c51ff92046cf50d7c5ba3dbe378f1c05adb9ef
zhumagulovt <60291721+zhumagulovt@users.noreply.github.com>
2022-09-08 15:32:26 +0600

    Delete posts directory

--- posts/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
-
-
-class IsOwnerOrReadOnly(BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return request.user == obj.author
--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-
-urlpatterns = [
-    path('', PostAPIList.as_view()),
-    path('<int:pk>/', PostAPIUpdate.as_view())
-]
